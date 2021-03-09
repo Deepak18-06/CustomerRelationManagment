2fd7412a270da3fb4edce10083db63b04ffe0839
Deepak kumar Prajapat <it1716.iiitbhopal@gmail.com>
2021-03-09 23:10:19 +0530

    CRUD (create update)

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 from .models import *
+from .forms import OrderForm
 
 def home(request):
     orders = Order.objects.all()
@@ -27,3 +28,27 @@ def customer(request,pk):
     orders_count = orders.count()
     context = {'customer':customer,'orders':orders,'orders_count':orders_count}
     return render(request,'accounts/customer.html',context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+    order = Order.objects.get(id = pk)
+    form = OrderForm(instance=order)
+
+    if request.method == "POST":
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request,'accounts/order_form.html',context)
